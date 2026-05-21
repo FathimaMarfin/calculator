c0e70bdb0217fdef91d8a5fc3820cec6db411596
Marfin <fathimamarfin@gmail.com>
2026-05-21 14:55:56 +0530

    Update jwtConfig.js

--- backend/jwtConfig.js
+++ backend/jwtConfig.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 
-const secretKey = crypto.randomBytes(32).toString('hex');
+const secretKey = process.env.JWT_SECRET || crypto.randomBytes(32).toString('hex');
 
 module.exports = {
     secretKey
